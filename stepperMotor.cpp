a3260d236232a1d72f10307915717442b6711572
brodyschwar <brodyschwartz@gmail.com>
2021-04-29 15:59:45 -0500

    please work

--- stepperMotor.cpp
+++ stepperMotor.cpp
@@ -1,70 +1,55 @@
 #include <stdio.h>
 #include <pigpio.h>
+#include <chrono>
+#include <thread>
+#include "stepperMotor.h"
 
-using std::string;
+using namespace std::this_thread; // sleep_for, sleep_until
+using namespace std::chrono; // nanoseconds, system_clock, seconds
 
-class StepperMotor {
-private:
-	const int stepPin;
-	const int dirPin;	
-	const int msPins[3];
-	const bool changePins;	
-	const double deg;
-	
-	double speed;
-	bool dir;
-	
-public:
 
-	StepperMotor(int sPin, int dirPin) : stepPin(sPin), 
-										 dirPin(dirPin),
-										 deg(1.8),
-										 msPins(),
-										 changePins(false)
-										 speed(1)
-										 dir(true) {}
+StepperMotor::StepperMotor(int sPin, int dirPin) :
+stepPin(sPin), dirPin(dirPin), msPins(), changePins(false), deg(), speed(), dir() {
+	gpioSetMode(sPin, PI_OUTPUT);
+	gpioSetMode(dirPin, PI_OUTPUT);
+}
 	
-	StepperMotor(int sPin, int dirPin, int d) : stepPin(sPin), 
-												dirPin(dirPin),
-												deg(d),
-												msPins(),
-												changePins(false) {}
+StepperMotor::StepperMotor(int sPin, int dirPin, int d) : 
+stepPin(sPin), dirPin(dirPin), msPins(), changePins(false), deg(d),  speed(), dir(){
+	gpioSetMode(sPin, PI_OUTPUT);
+	gpioSetMode(dirPin, PI_OUTPUT);
+}
 	
 	
-	double getSpeed() {
-		return speed;
+double StepperMotor::getSpeed() {
+	return speed;
+}
+	
+bool StepperMotor::getDir() {
+	return dir;
+}
+	
+void StepperMotor::setSpeed(double s) {
+	speed = s;
+}
+	
+void StepperMotor::setDir(int d) {
+	if (d == 1 || d == 0) dir = d;
+	else throw -1;
+}
+	
+void StepperMotor::changeDir() {
+	dir = (dir * -1) + 1;
+}
+	
+void StepperMotor::turn(double rot) {
+	int delay = ((360/(speed*deg))*1000000000);
+	int steps = rot*deg/360;
+	for (int i = 0; i < steps; ++i) {
+		gpioWrite(dirPin, dir);
+		gpioWrite(stepPin, 1);
+		sleep_for(nanoseconds(delay));
+		gpioWrite(stepPin,0);
+		sleep_for(nanoseconds(delay));
 	}
-	
-	string getDir() {
-		if(dir) return "CW";
-		else return "CCW";
-	}
-	
-	void setSpeed(double s) {
-		speed = s;
-	}
-	
-	void setDir(string d) {
-		if (d == "CW") dir = true;
-		else if (d == "CCW") dir = false;
-		else throw 1;
-	}
-	
-	void changeDir() {
-		dir = !dir;
-	}
-	
-	void run() {
-	}
-	
-	void halt() {
-	}
-	
-	void turn(double rot) {
-		double delay = (360/(speed*deg));
-		int steps = rot*deg/360;
-		for (int i = 0; i < steps; ++i) {
-		//
-		}
-	}
-};
+}

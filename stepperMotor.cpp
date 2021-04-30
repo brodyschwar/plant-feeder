00260ddb3b7c2a705f04238fa78f56193be657df
brodyschwar <brodyschwartz@gmail.com>
2021-04-29 19:23:39 -0500

    finally compiles!

--- stepperMotor.cpp
+++ stepperMotor.cpp
@@ -2,54 +2,65 @@
 #include <pigpio.h>
 #include <chrono>
 #include <thread>
+
 #include "stepperMotor.h"
 
 using namespace std::this_thread; // sleep_for, sleep_until
 using namespace std::chrono; // nanoseconds, system_clock, seconds
 
-
-StepperMotor::StepperMotor(int sPin, int dirPin) :
-stepPin(sPin), dirPin(dirPin), msPins(), changePins(false), deg(), speed(), dir() {
-	gpioSetMode(sPin, PI_OUTPUT);
-	gpioSetMode(dirPin, PI_OUTPUT);
-}
+class StepperMotor {
+private:
+	const int stepPin;
+	const int dirPin;	
+	int msPins[3] = {0,0,0};
+	bool changePins;	
+	double deg;
 	
-StepperMotor::StepperMotor(int sPin, int dirPin, int d) : 
-stepPin(sPin), dirPin(dirPin), msPins(), changePins(false), deg(d),  speed(), dir(){
-	gpioSetMode(sPin, PI_OUTPUT);
-	gpioSetMode(dirPin, PI_OUTPUT);
-}
+	double speed = 1;
+	int dir = 1;
 	
-	
-double StepperMotor::getSpeed() {
-	return speed;
-}
-	
-bool StepperMotor::getDir() {
-	return dir;
-}
-	
-void StepperMotor::setSpeed(double s) {
-	speed = s;
-}
-	
-void StepperMotor::setDir(int d) {
-	if (d == 1 || d == 0) dir = d;
-	else throw -1;
-}
-	
-void StepperMotor::changeDir() {
-	dir = (dir * -1) + 1;
-}
-	
-void StepperMotor::turn(double rot) {
-	int delay = ((360/(speed*deg))*1000000000);
-	int steps = rot*deg/360;
-	for (int i = 0; i < steps; ++i) {
-		gpioWrite(dirPin, dir);
-		gpioWrite(stepPin, 1);
-		sleep_for(nanoseconds(delay));
-		gpioWrite(stepPin,0);
-		sleep_for(nanoseconds(delay));
+public:
+	StepperMotor(int sPin, int dirPin) : stepPin(sPin), dirPin(dirPin), msPins(), changePins(false), deg(), speed(), dir() {
+		gpioSetMode(sPin, PI_OUTPUT);
+		gpioSetMode(dirPin, PI_OUTPUT);
 	}
-}
+	
+	StepperMotor(int sPin, int dirPin, int d) : stepPin(sPin), dirPin(dirPin), msPins(), changePins(false), deg(d),  speed(), dir(){
+		gpioSetMode(sPin, PI_OUTPUT);
+		gpioSetMode(dirPin, PI_OUTPUT);
+	}
+	
+	
+	double getSpeed() {
+		return speed;
+	}
+	
+	bool getDir() {
+		return dir;
+	}
+	
+	void setSpeed(double s) {
+		speed = s;
+	}
+
+	void setDir(int d) {
+		if (d == 1 || d == 0) dir = d;
+		else throw -1;
+	}
+	
+	void changeDir() {
+		dir = (dir * -1) + 1;
+	}
+	
+	void turn(double rot) {
+		int delay = ((360/(speed*deg))*1000000000);
+		int steps = rot*deg/360;
+		for (int i = 0; i < steps; ++i) {
+			gpioWrite(dirPin, dir);
+			gpioWrite(stepPin, 1);
+			sleep_for(nanoseconds(delay));
+			gpioWrite(stepPin,0);
+			sleep_for(nanoseconds(delay));
+		}
+	}
+};
--- stepperMotor.h
+++ stepperMotor.h
@@ -1,1 +1,25 @@
-/home/pi/stepperMotor.h
+#ifdef STEPPERMOTOR_H
+#define STEPPERMOTOR_H
+
+
+class StepperMotor {
+public:
+
+	StepperMotor::StepperMotor(int sPin, int dirPin);
+	
+	StepperMotor::StepperMotor(int sPin, int dirPin, int d);
+	
+	double StepperMotor::getSpeed() const;
+	
+	bool StepperMotor::getDir() const;
+	
+	void StepperMotor::setSpeed(double s);
+	
+	void StepperMotor::setDir(int d);
+	
+	void StepperMotor::changeDir();
+	
+	void StepperMotor::turn(double rot) const;
+};
+
+#endif
